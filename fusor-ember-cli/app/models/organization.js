fd573965fcd44e264e5261dcf8effc4600d35d25
Joseph Magen <joseph@isratrade.co.il>
2015-02-04 15:40:04 +0200

    remove label for organizations model

--- fusor-ember-cli/app/models/organization.js
+++ fusor-ember-cli/app/models/organization.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   title: DS.attr('string'),
-  label: DS.attr('string'),
+//  label: DS.attr('string'), only in Katello
   description: DS.attr('string')
 //  lifecycle_environments: DS.hasMany('lifecycle-environment', { async: true }),
 //  subnets: DS.hasMany('subnet', { async: true })
