8a73bb0dac1bf2b1d9918b96a3e7472eea7cb732
jesus m. rodriguez <jesusr@redhat.com>
2015-02-18 22:48:02 +0000

    rename columns and include missing columns

--- server/db/migrate/20150216190947_add_rhev_params.rb
+++ server/db/migrate/20150216190947_add_rhev_params.rb
@@ -3,12 +3,14 @@ class AddRhevParams < ActiveRecord::Migration
     remove_column :fusor_deployments, :rhev_params
     add_column :fusor_deployments, :rhev_hypervisor_host_id, :integer
     add_column :fusor_deployments, :rhev_engine_host_id, :integer
-    add_column :fusor_deployments, :rhev_hypervisor, :string
-    add_column :fusor_deployments, :rhev_engine, :string
-    add_column :fusor_deployments, :rhev_database, :string
-    add_column :fusor_deployments, :rhev_cluster, :string
-    add_column :fusor_deployments, :rhev_storage, :string
-    add_column :fusor_deployments, :rhev_cpu, :string
-    add_column :fusor_deployments, :rhev_share, :string
+    add_column :fusor_deployments, :rhev_hypervisor_hostname, :string
+    add_column :fusor_deployments, :rhev_engine_hostname, :string
+    add_column :fusor_deployments, :rhev_database_name, :string
+    add_column :fusor_deployments, :rhev_cluster_name, :string
+    add_column :fusor_deployments, :rhev_storage_name, :string
+    add_column :fusor_deployments, :rhev_storage_type, :string
+    add_column :fusor_deployments, :rhev_storage_address, :string
+    add_column :fusor_deployments, :rhev_cpu_type, :string
+    add_column :fusor_deployments, :rhev_share_path, :string
   end
 end
