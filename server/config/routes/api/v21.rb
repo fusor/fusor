896c462530570f30b988ded3d59cda286e56dad5
Joseph Magen <joseph@isratrade.co.il>
2015-02-25 11:50:50 +0200

    fix duplicate namespace /fusor/fusor/ to only be /fusor/

--- server/config/routes/api/v21.rb
+++ server/config/routes/api/v21.rb
@@ -1,5 +1,5 @@
 Fusor::Engine.routes.draw do
-  scope :fusor, :path => '/fusor' do
+  scope :fusor, :path => '/' do
     namespace :api do
       scope "(:api_version)", :module => :v21, :defaults => {:api_version => 'v21'}, :api_version => /v21/, :constraints => ApiConstraints.new(:version => 21, :default => false) do
         api_resources :deployments
