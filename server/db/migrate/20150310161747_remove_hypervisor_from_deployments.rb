c2fdef0cfbb544e760d9a2f1ab05e19a08693165
Joseph Magen <joseph@isratrade.co.il>
2015-03-19 09:30:34 +0200

    fixed duplication migration

--- server/db/migrate/20150310161747_remove_hypervisor_from_deployments.rb
+++ server/db/migrate/20150310161747_remove_hypervisor_from_deployments.rb
@@ -2,12 +2,10 @@ class RemoveHypervisorFromDeployments < ActiveRecord::Migration
   def up
     remove_column :fusor_deployments, :rhev_hypervisor_hostname
     remove_column :fusor_deployments, :rhev_hypervisor_host_id
-    add_column :fusor_deployments, :rhev_is_self_hosted, :boolean, :default => false
   end
 
   def down
     add_column :fusor_deployments, :rhev_hypervisor_hostname, :string
     add_column :fusor_deployments, :rhev_hypervisor_host_id, :string
-    remove_column :fusor_deployments, :rhev_is_self_hosted
   end
 end
