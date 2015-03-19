9c7062bff9a602c112f89359b5f8328884fb06c1
Joseph Magen <joseph@isratrade.co.il>
2015-03-19 12:00:08 +0200

    fixed deployment_serializer to remove deleted attributes :rhev_hypervisor_host_id and :rhev_hypervisor_hostname

--- server/app/serializers/fusor/deployment_serializer.rb
+++ server/app/serializers/fusor/deployment_serializer.rb
@@ -4,8 +4,7 @@ module Fusor
     embed :ids, include: true
     attributes :id, :name, :description,
                :deploy_rhev, :deploy_cfme, :deploy_openstack,
-               :rhev_hypervisor_host_id,
-               :rhev_hypervisor_hostname, :rhev_engine_hostname, :rhev_engine_admin_password,
+               :rhev_engine_hostname, :rhev_engine_admin_password,
                :rhev_database_name, :rhev_cluster_name, :rhev_storage_name,
                :rhev_storage_type, :rhev_storage_address, :rhev_cpu_type, :rhev_share_path,
                :rhev_is_self_hosted, :cfme_install_loc,
