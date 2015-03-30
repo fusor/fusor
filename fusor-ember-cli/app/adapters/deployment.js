c95472f7d05c7e91bfbfdd37160d2bf5de0ca24a
Joseph Magen <jmagen@redhat.com>
2015-03-30 15:50:14 +0300

    fix csrf token bug in FusorUI

--- fusor-ember-cli/app/adapters/deployment.js
+++ fusor-ember-cli/app/adapters/deployment.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
+var token = $('meta[name="csrf-token"]').attr('content');
 export default DS.ActiveModelAdapter.extend({
     namespace: 'fusor/api/v21',
+    headers: {
+        "X-CSRF-Token": token
+    }
 });
