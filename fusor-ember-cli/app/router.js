34bf43783fd46a88457355c1d66f48da4541da09
Joseph Magen <jmagen@redhat.com>
2015-05-18 13:47:29 +0300

    upgrade to ember-cli 0.2.5 and emberJS 1.12.0

--- fusor-ember-cli/app/router.js
+++ fusor-ember-cli/app/router.js
@@ -9,7 +9,7 @@ var Router = Ember.Router.extend({
   LOG_VIEW_LOOKUPS: true
 });
 
-export default Router.map(function() {
+Router.map(function() {
   this.route('login');
   this.route('loggedin');
 
@@ -100,3 +100,5 @@ export default Router.map(function() {
     this.resource("discovered-host", { path: '/:discovered_hosts_id' });
   });
 });
+
+export default Router;
