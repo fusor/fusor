9635eca3f9eae2fe43802a2a9951229363f5d19f
Jeffrey Phillips <jephilli@redhat.com>
2015-05-22 10:56:47 -0400

    Updates to base-f and subs to allow units, move required indicator to left
    Update base-f to:
    1) put asterisk to the left of the label when required
    2) allow for a ‘units’ label after the input area
    
    Update base-f based components to pass along
    isRequired, unitsLabel, and unitsClassSize

--- fusor-ember-cli/app/components/base-f.js
+++ fusor-ember-cli/app/components/base-f.js
@@ -6,5 +6,11 @@ export default Ember.Component.extend({
   }.property(),
   inputClassSize: function () {
     return this.getWithDefault('inputSize', 'col-md-4');
+  }.property(),
+  showUnits: function() {
+    return this.get('unitsLabel') !== undefined;
+  }.property('unitsLabel'),
+  unitsClassSize: function () {
+    return this.getWithDefault('unitsSize', 'col-md-2');
   }.property()
 });
