d0d65455fd3f82c1c0241937254b2fb47a004ae7
Joseph Magen <jmagen@redhat.com>
2015-05-25 17:12:03 +0300

    use Ember.isBlank instead of !== undefined

--- fusor-ember-cli/app/components/base-f.js
+++ fusor-ember-cli/app/components/base-f.js
@@ -1,15 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   labelClassSize: function () {
     return this.getWithDefault('labelSize', 'col-md-2');
   }.property(),
+
   inputClassSize: function () {
     return this.getWithDefault('inputSize', 'col-md-4');
   }.property(),
+
   showUnits: function() {
-    return this.get('unitsLabel') !== undefined;
+    return Ember.isBlank(this.get('unitsLabel'));
   }.property('unitsLabel'),
+
   unitsClassSize: function () {
     return this.getWithDefault('unitsSize', 'col-md-2');
   }.property()
