ec4012b8ab1b0137f77dd6c5721753d036c0bcf3
Jeffrey Phillips <jephilli@redhat.com>
2015-05-29 17:14:47 -0400

    Styling Updates

--- fusor-ember-cli/Brocfile.js
+++ fusor-ember-cli/Brocfile.js
@@ -21,4 +21,5 @@ var app = new EmberApp({
 // along with the exports of each module as its value.
 
 app.import('bower_components/jquery-csv/src/jquery.csv.js');
+app.import('bower_components/patternfly/dist/css/patternfly.min.css');
 module.exports = app.toTree();
