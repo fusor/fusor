1436378cb730c4a72ed578e72680e043ea0bab74
Joseph Magen <jmagen@redhat.com>
2015-05-29 15:02:58 +0300

    fix default RHEV Storage to NFS

--- fusor-ember-cli/app/controllers/storage.js
+++ fusor-ember-cli/app/controllers/storage.js
@@ -10,10 +10,6 @@ export default Ember.Controller.extend({
   step3RouteName: Ember.computed.alias("controllers.deployment.step3RouteName"),
   isCloudForms: Ember.computed.alias("controllers.deployment.isCloudForms"),
 
-  //defaults
-  rhev_storage_type: 'NFS',
-  rhev_gluster_ssh_port: 22,
-
   isNFS: function() {
     return (this.get('rhev_storage_type') === 'NFS');
   }.property('rhev_storage_type'),
