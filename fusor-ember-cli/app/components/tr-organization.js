be62e7fade050b3a6aeff7ef073c63d872b3c951
Joseph Magen <jmagen@redhat.com>
2015-06-08 21:04:55 +0300

    style selected organization with blue background

--- fusor-ember-cli/app/components/tr-organization.js
+++ fusor-ember-cli/app/components/tr-organization.js
@@ -4,9 +4,17 @@ export default Ember.Component.extend({
 
   tagName: 'tr',
 
+  classNameBindings: ['bgColor'],
+
   isChecked: function () {
     return (this.get('selectedOrganization') == this.get('org'));
   }.property('selectedOrganization', 'org'),
+
+  bgColor: function () {
+    if (this.get('isChecked')) {
+      return 'white-on-blue';
+    }
+  }.property('isChecked'),
 
   actions: {
     organizationChanged: function(event) {
