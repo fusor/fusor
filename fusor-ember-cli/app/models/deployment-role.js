7a0af2fd4ba7c0156e5c0afac6359d035cb5effd
Jeffrey Phillips <jephilli@redhat.com>
2015-07-14 16:16:31 -0400

    Fixes for deployment role coloring by type, add edit deployment role dialog

--- fusor-ember-cli/app/models/deployment-role.js
+++ fusor-ember-cli/app/models/deployment-role.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  description: DS.attr('string'),
   version: DS.attr('number'),
 
   countParameterName: function() {
@@ -9,5 +10,26 @@ export default DS.Model.extend({
   }.property('name', 'version'),
   flavorParameterName: function() {
       return this.get('name') + '-' + this.get('version') + '::Flavor'
-  }.property('name', 'version')
+  }.property('name', 'version'),
+
+  roleType: function() {
+    var name = this.get('name').toLowerCase();
+    if (name.indexOf('controller') >= 0) {
+      return 'controller';
+    }
+    else if (name.indexOf('compute') >= 0) {
+      return 'compute';
+    }
+    else if (name.indexOf('cinder') >= 0) {
+      return 'block';
+    }
+    else if (name.indexOf('swift') >= 0) {
+      return 'object';
+    }
+    else if (name.indexOf('ceph') >= 0) {
+      return 'block';
+    }
+
+    return name;
+  }.property('name')
 });
