29e8811a29a444e06787826795b6f6d03d274312
Jeffrey Phillips <jephilli@redhat.com>
2015-07-22 11:39:07 -0400

    Update edit deployment role dialog to show service config parameters

--- fusor-ember-cli/app/models/deployment-plan-parameter.js
+++ fusor-ember-cli/app/models/deployment-plan-parameter.js
@@ -3,4 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     description: DS.attr('string'),
     value: DS.attr('string'),
+    hidden: DS.attr('boolean'),
+    parameter_type: DS.attr('string')
 });
