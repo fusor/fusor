4d39f2fd866478c3efe06be47f6562593d78863d
Jeffrey Phillips <jephilli@redhat.com>
2015-07-22 11:41:00 -0400

    Add initial undercloud deploy page

--- fusor-ember-cli/app/router.js
+++ fusor-ember-cli/app/router.js
@@ -51,6 +51,7 @@ export default Router.map(function() {
     });
 
     this.resource('openstack', function() {
+      this.resource('undercloud-deploy');
       this.resource('register-nodes');
       this.resource('assign-nodes');
     });
