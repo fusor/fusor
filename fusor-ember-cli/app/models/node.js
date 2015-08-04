0ed523f3c212c25f42a04fd4930eb286d553b6a2
Jeffrey Phillips <jephilli@redhat.com>
2015-08-04 07:57:03 -0400

    Update for Register Nodes progress to check introspection status

--- fusor-ember-cli/app/models/node.js
+++ fusor-ember-cli/app/models/node.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+    uuid: DS.attr('string'),
     driver: DS.attr('string'),
     provision_state: DS.attr('string'),
     driver_info: DS.attr(),
