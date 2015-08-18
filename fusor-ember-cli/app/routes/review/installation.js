c789e81fea08f7c0a0b1622c76c6aaf3f41526cd
Joseph Magen <jmagen@redhat.com>
2015-08-19 00:09:04 +0300

    show domain name for hostgroup RHEV-Engine and RHEV-Hypervisor on review tab

--- fusor-ember-cli/app/routes/review/installation.js
+++ fusor-ember-cli/app/routes/review/installation.js
@@ -9,6 +9,12 @@ export default Ember.Route.extend({
     controller.set('model', model);
     controller.set('showSpinner', false);
     controller.set('showErrorMessage', false);
+    this.store.find('hostgroup').then(function(results) {
+        var engineDomain = results.filterBy('name', 'RHEV-Engine').get('firstObject').get('domain.name');
+        var hypervisorDomain = results.filterBy('name', 'RHEV-Hypervisor').get('firstObject').get('domain.name');
+        controller.set('engineDomain', engineDomain);
+        controller.set('hypervisorDomain', hypervisorDomain);
+    })
   }
 
 });
