fd2101adda82a55e51b8aadbf036549cd9a231e9
Joseph Magen <jmagen@redhat.com>
2015-08-19 00:09:04 +0300

    show domain name for hostgroup RHEV-Engine and RHEV-Hypervisor on review tab

--- fusor-ember-cli/app/components/hypervisor-name.js
+++ fusor-ember-cli/app/components/hypervisor-name.js
@@ -4,13 +4,11 @@ export default Ember.Component.extend({
 
   namePlusDomain: function() {
     if (this.get("host.is_discovered")) {
-      // need to add domain for discovered host to make fqdn
-      // TODO - dynamically get domain name of hostgroup Fusor Base if is not example.com
-      return (this.get("host.name") + '.example.com');
+      return (this.get("host.name") + "." + this.get('hypervisorDomain'));
     } else {
       // name is fqdn for managed host
       return (this.get("host.name"));
     }
-  }.property('host')
+  }.property('host', 'hypervisorDomain')
 
 });
