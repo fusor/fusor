df1c0021d23907a006e1ddf3c4149cf6926a9af1
Joseph Magen <jmagen@redhat.com>
2015-10-03 00:11:21 +0300

    fix .popovers() to .popover()

--- fusor-ember-cli/app/components/base-popover.js
+++ fusor-ember-cli/app/components/base-popover.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement: function(){
-      return Ember.$('[data-toggle=popover]').popovers();
+      return Ember.$('[data-toggle=popover]').popover();
   }
 });
