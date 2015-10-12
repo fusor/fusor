9bfb8722572cf5a13cfbd01e2f8220278418f138
Joseph Magen <jmagen@redhat.com>
2015-10-12 11:47:32 +0300

    add icons for VM or Bare Metel in Engine/Hypervisor tables

---
+++ fusor-ember-cli/app/helpers/host-type.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function hostType(host) {
+    if (this.get('host.is_virtual')) {
+      return new Ember.Handlebars.SafeString('<center><img src="/assets/r/vm-icon-16.png" class="img-responsive" style="margin-top: 3px"></center>');
+    } else {
+      return new Ember.Handlebars.SafeString('<span class="pficon pficon-screen" style="font-size: 16px; margin-top: 3px"></span>');
+    }
+}
+
+export default Ember.HTMLBars.makeBoundHelper(hostType);
