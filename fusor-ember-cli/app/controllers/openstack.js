2a982ae73136cc156f155f8a974a0f321362ed5c
Joseph Magen <jmagen@redhat.com>
2015-10-15 16:43:23 +0300

    fix when refresh the OSP pages, the side navigation tabs remain enabled as they should and don't grey out

--- fusor-ember-cli/app/controllers/openstack.js
+++ fusor-ember-cli/app/controllers/openstack.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   needs: ['deployment', 'undercloud-deploy'],
 
   stepNumberOpenstack: Ember.computed.alias("controllers.deployment.stepNumberOpenstack"),
-  disableTabRegisterNodes: Ember.computed.alias("controllers.undercloud-deploy.disableTabRegisterNodes"),
-  disableTabAssignNodes: Ember.computed.alias("controllers.undercloud-deploy.disableTabAssignNodes"),
+  disableTabRegisterNodes: Ember.computed.empty("model.openstack_undercloud_password"),
+  disableTabAssignNodes: Ember.computed.empty("model.openstack_undercloud_password"),
   validOpenStack: true //TODO
 });
