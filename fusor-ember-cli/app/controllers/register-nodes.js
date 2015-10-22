461f4d40b346d4c3f02270bcaf490130cdef9726
Joseph Magen <jmagen@redhat.com>
2015-10-22 10:58:18 +0300

    refresh model.nodes and model.profiles so page auto-updates after registering new node

--- fusor-ember-cli/app/controllers/register-nodes.js
+++ fusor-ember-cli/app/controllers/register-nodes.js
@@ -121,7 +121,7 @@ export default Ember.Controller.extend({
     var stepsComplete = (nodeRegComplete * 4) + (nodesIntrospection * 1);
 
     return Math.round(stepsComplete / numSteps * 100);
-  }.property('nodeRegComplete', 'nodeRegTotal', 'newNodes', 'introspectionNodes'),
+  }.property('nodeRegComplete', 'nodeRegTotal', 'newNodes.[]', 'introspectionNodes.[]'),
 
   noRegisteredNodes: function() {
       return (this.get('model.nodes.length') < 1);
@@ -175,6 +175,20 @@ export default Ember.Controller.extend({
   },
 
   actions: {
+
+    refreshNodesAndFlavors: function() {
+      // manually set manual rather than using this.get('model').reload() which looks at data store changes
+      // since the nodes changes or db changes happened outside of ember-data.
+      console.log('refreshing model.nodes and model.profiles');
+      var deploymentId = this.get('deploymentId');
+      var self = this;
+      Ember.RSVP.hash({nodes: this.store.find('node', {deployment_id: deploymentId}),
+                       profiles: this.store.find('flavor', {deployment_id: deploymentId})
+                     }).then(function(result) {
+                         return self.set('model', result);
+                     });
+    },
+
     showNodeRegistrationModal: function() {
       var newNodes = this.get('newNodes');
       var errorNodes = this.get('errorNodes');
@@ -464,6 +478,9 @@ export default Ember.Controller.extend({
 
     var promiseFunction = function(resolve) {
       var checkForDone = function() {
+        //refresh model
+        self.send('refreshNodesAndFlavors');
+
         //ic-ajax request
         console.log('action: checkNodeIntrospection');
         console.log('POST /fusor/api/openstack/deployments/' + self.get('deploymentId') + '/nodes/' + node.uuid + '/ready');
