8ba4d639d130b1a2386d0914d06455247963277c
Chris Chase <chris@chasenc.com>
2015-11-06 16:01:43 -0500

    Fixed paths on mixin imports.

--- fusor-ember-cli/app/controllers/review/progress.js
+++ fusor-ember-cli/app/controllers/review/progress.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import NeedsDeploymentMixin from "../../../mixins/needs-deployment-mixin";
+import NeedsDeploymentMixin from "../../mixins/needs-deployment-mixin";
 
 export default Ember.Controller.extend(NeedsDeploymentMixin, {
 
