92f32cfb17f17ec6a572668ed3a9ba609c24a643
Joseph Magen <jmagen@redhat.com>
2015-11-12 16:28:38 +0200

    first unit tests on deployment model

--- fusor-ember-cli/tests/unit/models/deployment-test.js
+++ fusor-ember-cli/tests/unit/models/deployment-test.js
@@ -1,12 +1,44 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('deployment', 'Unit | Model | deployment', {
   // Specify the other units that are required for this test.
-  needs: ['model:organization', 'model:lifecycle-environment', 'model:discovered-host', 'model:subscription']
+  needs: ['model:organization',
+          'model:lifecycle-environment',
+          'model:discovered-host',
+          'model:subscription',
+          'model:introspection-task',
+          'model:foreman-task',
+          'adapter:application'
+         ]
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+test('isStarted should be false on init', function(assert){
+  var model = this.subject();
+  assert.equal(model.get('isStarted'), false);
+  assert.equal(model.get('isNotStarted'), true);
+});
+
+test('isStarted should be true if foreman_task_uuid is set to a valid uuid', function(assert){
+  var model = this.subject();
+  Ember.run(function() {
+    model.set('foreman_task_uuid', 'db25a76f-e344-48ba-ac77-f29303586dbe');
+  });
+  assert.equal(model.get('isStarted'), true);
+  assert.equal(model.get('isNotStarted'), false);
+});
+
+test('isStarted should be false if foreman_task_uuid is null', function(assert){
+  var model = this.subject();
+  Ember.run(function() {
+    model.set('foreman_task_uuid', null);
+  });
+  assert.equal(model.get('isStarted'), false);
+  assert.equal(model.get('isNotStarted'), true);
 });
