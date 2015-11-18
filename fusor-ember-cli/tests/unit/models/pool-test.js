6bd2f9c9bddba917ea4a427ef5a961a713901987
Joseph Magen <jmagen@redhat.com>
2015-11-18 17:29:37 +0200

    add unit tests for model pool

--- fusor-ember-cli/tests/unit/models/pool-test.js
+++ fusor-ember-cli/tests/unit/models/pool-test.js
@@ -5,3 +5,36 @@ moduleForModel('pool', 'Unit | Model | pool', {
   needs: []
 });
 
+test('it exists', function(assert) {
+  var model = this.subject();
+  // var store = this.store();
+  assert.ok(!!model);
+});
+
+test('qtyAvailable is calculated corrected', function(assert){
+  var model = this.subject();
+  Ember.run(function() {
+    model.set('quantity', 10);
+    model.set('consumed', 3);
+  });
+  assert.equal(model.get('qtyAvailable'), 7);
+});
+
+test('qtyAvailableOfTotal is Unlimited if qtyAvailable returns -1', function(assert){
+  var model = this.subject();
+  Ember.run(function() {
+    model.set('quantity', 0);
+    model.set('consumed', 1);
+  });
+  assert.equal(model.get('qtyAvailable'), -1);
+  assert.equal(model.get('qtyAvailableOfTotal'), "Unlimited");
+});
+
+test('qtyAvailableOfTotal is correctly outputed', function(assert){
+  var model = this.subject();
+  Ember.run(function() {
+    model.set('quantity', 100);
+    model.set('consumed', 55);
+  });
+  assert.equal(model.get('qtyAvailableOfTotal'), "45 of 100");
+});
