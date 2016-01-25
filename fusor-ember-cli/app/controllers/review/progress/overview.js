eae770d52311b2399c64164e08b1a80a10d17167
Joseph Magen <jmagen@redhat.com>
2016-01-25 14:12:27 +0200

    show tasks that were stopped + show parent Deploy task if subtasks are 100%

--- fusor-ember-cli/app/controllers/review/progress/overview.js
+++ fusor-ember-cli/app/controllers/review/progress/overview.js
@@ -25,4 +25,33 @@ export default Ember.Controller.extend(ProgressBarMixin, NeedsDeploymentMixin, {
     return ((this.get('progressDeployment') === '1') && (this.get('resultDeployment') === 'success'));
   }),
 
+  //Deploy task is not 100% but All subtasks are 100%
+  showDeployTaskProgressBar: Ember.computed('isRhev',
+                                            'isOpenStack',
+                                            'isCloudForms',
+                                            'manageContentTask.progress',
+                                            'rhevTask.progress',
+                                            'openstackTask.progress',
+                                            'cfmeTask.progress',
+                                            'progressDeployment',
+                                            function() {
+    if (this.get('progressDeployment') === '1' || this.get('manageContentTask.progress') !== '1') {
+      return false;
+    }
+
+    if (this.get('isRhev') && this.get('rhevTask.progress') !== '1') {
+      return false;
+    }
+
+    if (this.get('isOpenStack') && this.get('openstackTask.progress') !== '1') {
+      return false;
+    }
+
+    if (this.get('isCloudForms') && this.get('cfmeTask.progress') !== '1') {
+      return false;
+    }
+
+    return true;
+  })
+
 });
