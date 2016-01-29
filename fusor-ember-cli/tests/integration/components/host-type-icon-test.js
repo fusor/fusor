41183bbb44d132a357a2ea2576e48f1eb36a838b
Chris Chase <chris@chasenc.com>
2016-01-29 11:45:22 -0500

    1300821 - Invert virtual machine icon on selected hypervisors. Moved inline styles to css file.

--- fusor-ember-cli/tests/integration/components/host-type-icon-test.js
+++ fusor-ember-cli/tests/integration/components/host-type-icon-test.js
@@ -5,12 +5,26 @@ moduleForComponent('host-type-icon', 'Integration | Component | host type icon',
   integration: true
 });
 
-test('it show vm-icon-16 image if isVM is true', function(assert) {
+test('it shows vm-icon-16 image and hides pficon-screen if isVM is true', function(assert) {
   this.render(hbs`{{host-type-icon isVM=true}}`);
   assert.equal(this.$('img').attr('src'), '/assets/r/vm-icon-16.png');
+  assert.notOk(this.$('span.pficon-screen')[0]);
 });
 
-test('it show pficon-screen icon if isVM is false', function(assert) {
+test('it shows vm-icon-16 image and hides pficon-screen if isVM is true and isInverted is false', function(assert) {
+  this.render(hbs`{{host-type-icon isVM=true isInverted=false}}`);
+  assert.equal(this.$('img').attr('src'), '/assets/r/vm-icon-16.png');
+  assert.notOk(this.$('span.pficon-screen')[0]);
+});
+
+test('it shows vm-icon-inverted-16 image and hides pficon-screen if isVM is true and isInverted is true', function(assert) {
+  this.render(hbs`{{host-type-icon isVM=true isInverted=true}}`);
+  assert.equal(this.$('img').attr('src'), '/assets/r/vm-icon-inverted-16.png');
+  assert.notOk(this.$('span.pficon-screen')[0]);
+});
+
+test('it shows pficon-screen icon and hides images if isVM is false', function(assert) {
   this.render(hbs`{{host-type-icon isVM=false}}`);
-  assert.ok(this.$('span.pficon-screen'));
+  assert.ok(this.$('span.pficon-screen')[0]);
+  assert.notOk(this.$('img').attr('src'));
 });
