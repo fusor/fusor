e86a110fad1b9d7a233c100f7e72bc48b11c421d
jkim <johnkim76@gmail.com>
2016-03-16 00:29:10 -0400

    add vm_launcher and launch openshift vms

--- server/app/lib/actions/fusor/deployment/rhev/ose_launch.rb
+++ server/app/lib/actions/fusor/deployment/rhev/ose_launch.rb
@@ -28,9 +28,43 @@ module Actions
           def run
             ::Fusor.log.debug '====== OSE Launch run method ======'
             deployment = ::Fusor::Deployment.find(input[:deployment_id])
-            compute_attrs = create_compute_profile(deployment).vm_attrs
-            host = create_host(deployment, compute_attrs)
-            #TODO save host.ip and host.name in deployment
+
+            # launch master nodes
+            for i in 1..deployment.openshift_number_master_nodes do
+              vmlauncher = Utils::Fusor::VMLauncher.new(deployment, 
+                                                        'ose', 
+                                                        deployment.openshift_install_loc,
+                                                        'RedHat 7.1',
+                                                        'x86_64')
+              vmlauncher.set_hostname("#{deployment.label.tr('_', '-')}-ose-master#{i}")
+              
+              host = vmlauncher.launch_openshift_vm(deployment.openshift_master_vcpu,
+                                                    deployment.openshift_master_ram,
+                                                    deployment.openshift_master_disk,
+                                                    deployment.openshift_storage_size)
+              ::Fusor.log.debug "====== OSE Launched VM Name : #{host.name} ======"
+              ::Fusor.log.debug "====== OSE Launched VM IP   : #{host.ip}   ======"
+              #TODO save host.ip and host.name in deployment
+            end
+
+            # launch worker nodes
+            for i in 1..deployment.openshift_number_worker_nodes do
+              vmlauncher = Utils::Fusor::VMLauncher.new(deployment, 
+                                                        'ose', 
+                                                        deployment.openshift_install_loc,
+                                                        'RedHat 7.1',
+                                                        'x86_64')
+              vmlauncher.set_hostname("#{deployment.label.tr('_', '-')}-ose-node#{i}")
+              
+              host = vmlauncher.launch_openshift_vm(deployment.openshift_node_vcpu,
+                                                    deployment.openshift_node_ram,
+                                                    deployment.openshift_node_disk,
+                                                    deployment.openshift_storage_size)
+              ::Fusor.log.debug "====== OSE Launched VM Name : #{host.name} ======"
+              ::Fusor.log.debug "====== OSE Launched VM IP   : #{host.ip}   ======"
+              #TODO save host.ip and host.name in deployment
+            end
+
             ::Fusor.log.debug '====== Leaving OSE Launch run method ======'
           end
 
@@ -38,93 +72,8 @@ module Actions
             ::Fusor.log.info 'OSE Launch Completed'
           end
 
-          def ose_launch_failed
-            fail _('OSE Launch failed')
-          end
-
-          private
-
-          def create_compute_profile(deployment)
-            cp = ComputeProfile.create("name" => "#{deployment.label}-ose")
-            cr = ComputeResource.find_by_name("#{deployment.label}-RHEV")
-            cl_id = cr.clusters.find { |c| c.name == deployment.rhev_cluster_name }.id
-            net_id = cr.available_networks(cl_id).first.id
-            storage_id = cr.available_storage_domains(cl_id).first.id
-            template_id = cr.templates.find { |t| t.name == "#{deployment.label}-ose-template" }.id
-
-            ComputeAttribute.create({"compute_profile_id" => cp.id,
-                                     "compute_resource_id" => cr.id,
-                                     "vm_attrs" => {
-                                       "cluster" => cl_id,
-                                       "template" => template_id,
-                                       "cores" => 4,
-                                       "memory" => 6_442_450_944,
-                                       "interfaces_attributes" => {
-                                         "new_interfaces" => {
-                                           "name" => "",
-                                           "network" => net_id,
-                                           "delete" => ""
-                                         }.with_indifferent_access,
-                                         "0" => {
-                                           "name" => "eth0",
-                                           "network" => net_id,
-                                           "delete" => ""
-                                         }.with_indifferent_access
-                                       }.with_indifferent_access,
-                                       "volumes_attributes" => {
-                                         "new_volumes" => {
-                                           "size_gb" => "",
-                                           "storage_domain" => storage_id,
-                                           "_delete" => "",
-                                           "id" => "",
-                                           "preallocate" => "0"
-                                         }.with_indifferent_access,
-                                         "0" => {
-                                           "size_gb" => 40,
-                                           "storage_domain" => storage_id,
-                                           "_delete" => "",
-                                           "id" => "",
-                                           "preallocate" => "0"
-                                         }.with_indifferent_access,
-                                         "1" => {
-                                           "size_gb" => 50,
-                                           "storage_domain" => storage_id,
-                                           "_delete" => "",
-                                           "id" => "",
-                                           "preallocate" => "0"
-                                         }.with_indifferent_access
-                                       }.with_indifferent_access
-                                     }.with_indifferent_access
-                                   }.with_indifferent_access)
-          end
-
-          def create_host(deployment, compute_attrs)
-            hg_id = Hostgroup.where(name: deployment.label).first.id
-            ose = {"name" => "#{deployment.label.tr('_', '-')}-ose",
-                    "location_id" => Location.find_by_name('Default Location').id,
-                    "environment_id" => Environment.where(:katello_id => "Default_Organization/Library/Fusor_Puppet_Content").first.id,
-                    "organization_id" => deployment["organization_id"],
-                    "compute_resource_id" => ComputeResource.find_by_name("#{deployment.label}-RHEV").id,
-                    "enabled" => "1",
-                    "managed" => "1",
-                    "architecture_id" => Architecture.find_by_name('x86_64')['id'],
-                    "operatingsystem_id" => Operatingsystem.find_by_title('RedHat 7.1')['id'],
-                    "ptable_id" => Ptable.find { |p| p["name"] == "Kickstart default" }.id,
-                    "domain_id" => 1,
-                    "root_pass" => "dog8code",
-                    "mac" => "admin",
-                    "build" => "0",
-                    "hostgroup_id" => hg_id,
-                    #using a compute_profile_id the vm does not start, so for now merge with attr.
-                    "compute_attributes" => {"start" => "1"}.with_indifferent_access.merge(compute_attrs)}
-            host = ::Host.create(ose)
-
-            if host.errors.empty?
-              ose_launch_completed
-              return host
-            else
-              ose_launch_failed
-            end
+          def ose_launch_failed(hostname)
+            fail _('OSE Launch failed #{hostname}')
           end
         end
       end
