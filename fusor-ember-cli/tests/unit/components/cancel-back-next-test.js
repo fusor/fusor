b243fdf2216c78210c11ad2c59cac0b3c34e1c31
Joseph Magen <jmagen@redhat.com>
2016-04-25 10:28:23 +0300

    modal - cancel deployment modal from component cancel-back-next

--- fusor-ember-cli/tests/unit/components/cancel-back-next-test.js
+++ fusor-ember-cli/tests/unit/components/cancel-back-next-test.js
@@ -2,7 +2,7 @@ import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('cancel-back-next', 'Unit | Component | cancel back next', {
   // Specify the other units that are required for this test
-  needs: ['component:em-modal-toggler'],
+  needs: [],
   unit: true
 });
 
