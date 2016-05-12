184e4489861f381ab88ad1ddb3bcc05367cbb9a1
Erik Nelson <erik@nsk.io>
2016-05-12 10:35:49 -0400

    Adapter autoindent

--- fusor-ember-cli/app/adapters/application.js
+++ fusor-ember-cli/app/adapters/application.js
@@ -4,26 +4,26 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 var token = Ember.$('meta[name="csrf-token"]').attr('content');
 export default ActiveModelAdapter.extend({
-    namespace: 'api/v21',
-    headers: {
-        "X-CSRF-Token": token
-    },
-    shouldReloadRecord(store, ticketSnapshot) {
-      return true;
-    },
-    shouldReloadAll() {
-      return true;
-    },
-    handleResponse(status /*, headers, payload */) {
-        if(status === 401) {
-            this.eventBus.trigger('displayErrorModal', {
-                errorMessage: 'It looks like your session has timed out.' +
+  namespace: 'api/v21',
+  headers: {
+    "X-CSRF-Token": token
+  },
+  shouldReloadRecord(store, ticketSnapshot) {
+    return true;
+  },
+  shouldReloadAll() {
+    return true;
+  },
+  handleResponse(status /*, headers, payload */) {
+    if(status === 401) {
+      this.eventBus.trigger('displayErrorModal', {
+        errorMessage: 'It looks like your session has timed out.' +
                     ' Try logging back in again to continue.',
-                okayCallback: () => {
-                    document.location.pathname = '/'; // Redirect to root
-                }
-            });
+        okayCallback: () => {
+          document.location.pathname = '/'; // Redirect to root
         }
-            return this._super.apply(this, arguments);
+      });
     }
+    return this._super.apply(this, arguments);
+  }
 });
--- fusor-ember-cli/app/adapters/management-application.js
+++ fusor-ember-cli/app/adapters/management-application.js
@@ -2,11 +2,11 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
 
-    namespace: '',
-    urlForQuery(query, modelName) {
-        // Use owner key to get consumers (subscription application manangers)
-        // GET /customer_portal/owners/#{OWNER['key']}/consumers?type=satellite
-        return '/customer_portal/owners/' + query['owner_key'] + '/consumers?type=satellite';
-    }
+  namespace: '',
+  urlForQuery(query, modelName) {
+    // Use owner key to get consumers (subscription application manangers)
+    // GET /customer_portal/owners/#{OWNER['key']}/consumers?type=satellite
+    return '/customer_portal/owners/' + query['owner_key'] + '/consumers?type=satellite';
+  }
 
 });
