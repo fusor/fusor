7bc300a0e1d56d3720e9f74d004f37ffb1845f88
Erik Nelson <erik@nsk.io>
2016-05-12 13:32:32 -0400

    Serializer manual updates

--- fusor-ember-cli/app/serializers/entitlement.js
+++ fusor-ember-cli/app/serializers/entitlement.js
@@ -2,18 +2,18 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
 
-    // add root node 'entitlements' that customer protal JSON response doesn't return
+  // add root node 'entitlements' that customer protal JSON response doesn't return
   extractArray(store, type, payload) {
     payload = { entitlements: payload };
     return this._super(store, type, payload);
   },
 
-    // remove attribute keys in the json response that aren't in the model management application
+  // remove attribute keys in the json response that aren't in the model management application
   normalizeHash: {
     entitlements(hash) {
       delete hash.consumer;
       delete hash.certificates;
-        // move attributes within the 'pool' node to main level
+      // move attributes within the 'pool' node to main level
       hash.poolId = hash.pool.id;
       hash.poolType = hash.pool.type;
       hash.poolQuantity = hash.pool.quantity;
--- fusor-ember-cli/app/serializers/pool.js
+++ fusor-ember-cli/app/serializers/pool.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
 
-    // add root node 'entitlements' that customer protal JSON response doesn't return
+  // add root node 'entitlements' that customer protal JSON response doesn't return
   extractArray(store, type, payload) {
     payload = { pools: payload };
     return this._super(store, type, payload);
   },
 
-    // remove attribute keys in the json response that aren't in the model management application
+  // remove attribute keys in the json response that aren't in the model management application
   normalizeHash: {
     management_applications(hash) {
       delete hash.releaseVer;
