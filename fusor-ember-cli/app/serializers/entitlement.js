6518bb963db87a3b10c9116b93b0e14419f051f6
Erik Nelson <erik@nsk.io>
2016-05-12 13:32:01 -0400

    Serializers autoindent

--- fusor-ember-cli/app/serializers/entitlement.js
+++ fusor-ember-cli/app/serializers/entitlement.js
@@ -3,31 +3,31 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
 
     // add root node 'entitlements' that customer protal JSON response doesn't return
-    extractArray(store, type, payload) {
-      payload = { entitlements: payload };
-      return this._super(store, type, payload);
-    },
+  extractArray(store, type, payload) {
+    payload = { entitlements: payload };
+    return this._super(store, type, payload);
+  },
 
     // remove attribute keys in the json response that aren't in the model management application
-    normalizeHash: {
-      entitlements(hash) {
-        delete hash.consumer;
-        delete hash.certificates;
+  normalizeHash: {
+    entitlements(hash) {
+      delete hash.consumer;
+      delete hash.certificates;
         // move attributes within the 'pool' node to main level
-        hash.poolId = hash.pool.id;
-        hash.poolType = hash.pool.type;
-        hash.poolQuantity = hash.pool.quantity;
-        hash.subscriptionId = hash.pool.subscriptionId;
-        hash.activeSubscription = hash.pool.activeSubscription;
-        hash.contractNumber = hash.pool.contractNumber;
-        hash.accountNumber = hash.pool.accountNumber;
-        hash.consumed = hash.pool.consumed;
-        hash.exported = hash.pool.exported;
-        hash.consumed = hash.pool.consumed;
-        hash.productName = hash.pool.productName;
-        delete hash.pool;
-        return hash;
-      }
+      hash.poolId = hash.pool.id;
+      hash.poolType = hash.pool.type;
+      hash.poolQuantity = hash.pool.quantity;
+      hash.subscriptionId = hash.pool.subscriptionId;
+      hash.activeSubscription = hash.pool.activeSubscription;
+      hash.contractNumber = hash.pool.contractNumber;
+      hash.accountNumber = hash.pool.accountNumber;
+      hash.consumed = hash.pool.consumed;
+      hash.exported = hash.pool.exported;
+      hash.consumed = hash.pool.consumed;
+      hash.productName = hash.pool.productName;
+      delete hash.pool;
+      return hash;
     }
+  }
 
 });
--- fusor-ember-cli/app/serializers/pool.js
+++ fusor-ember-cli/app/serializers/pool.js
@@ -3,22 +3,22 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
 
     // add root node 'entitlements' that customer protal JSON response doesn't return
-    extractArray(store, type, payload) {
-      payload = { pools: payload };
-      return this._super(store, type, payload);
-    },
+  extractArray(store, type, payload) {
+    payload = { pools: payload };
+    return this._super(store, type, payload);
+  },
 
     // remove attribute keys in the json response that aren't in the model management application
-    normalizeHash: {
-      management_applications(hash) {
-        delete hash.releaseVer;
-        delete hash.type;
-        delete hash.owner;
-        delete hash.installedProducts;
-        delete hash.guestIds;
-        delete hash.capabilities;
-        return hash;
-      }
+  normalizeHash: {
+    management_applications(hash) {
+      delete hash.releaseVer;
+      delete hash.type;
+      delete hash.owner;
+      delete hash.installedProducts;
+      delete hash.guestIds;
+      delete hash.capabilities;
+      return hash;
     }
+  }
 
 });
