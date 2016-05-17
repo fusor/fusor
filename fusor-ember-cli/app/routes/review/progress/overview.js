6774de0b4182057c75cc267a9d37d65eca561dca
Joseph Magen <jmagen@redhat.com>
2016-05-17 15:37:37 +0300

    error fix in mocks-enabled environment

--- fusor-ember-cli/app/routes/review/progress/overview.js
+++ fusor-ember-cli/app/routes/review/progress/overview.js
@@ -57,6 +57,7 @@ export default Ember.Route.extend({
     // serverside once a given deployment has been initiated.
     ////////////////////////////////////////////////////////////
     let contentErrorDiscovered =
+      model.manageContentTask &&
       model.manageContentTask.get('result') === 'error' &&
       model.manageContentTask.get('state') === 'paused';
     ////////////////////////////////////////////////////////////
