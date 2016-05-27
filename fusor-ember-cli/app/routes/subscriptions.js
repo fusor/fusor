4fcda547c76358b9052463460fbaacb31a9104c6
Erik Nelson <erik@nsk.io>
2016-05-27 13:36:18 -0400

    More intelligent detection of existing manifest case
    - Covers some edge cases

--- fusor-ember-cli/app/routes/subscriptions.js
+++ fusor-ember-cli/app/routes/subscriptions.js
@@ -33,8 +33,44 @@ export default Ember.Route.extend({
   },
 
   shouldUseExistingManifest() {
-    const deplController = this.controllerFor('deployment');
-    return Ember.isPresent(deplController.get('model.upstream_consumer_uuid'));
+    const orgId = this.modelFor('deployment').get('organization.id');
+    const modelUpstreamConsumerUuid = this.controllerFor('deployment')
+      .get('model.upstream_consumer_uuid');
+    const hasModelUpstreamConsumerUuid = Ember.isPresent(modelUpstreamConsumerUuid);
+
+    return new Ember.RSVP.Promise((res, rej) => {
+      const url = `/katello/api/v2/organizations/${orgId}`;
+      Ember.$.getJSON(url).then(results => {
+        const satManifestExists =
+          Ember.isPresent(results.owner_details) &&
+          Ember.isPresent(results.owner_details.upstreamConsumer);
+
+        if(!satManifestExists && hasModelUpstreamConsumerUuid) {
+          // Edge case where an upstream_consumer_uuid has been saved into the
+          // fusor model but not yet uploaded to satellite. Indicates a deployment
+          // in progress, but not one where satellite already has an existing
+          // manifest available for reuse
+          res(false);
+        } else if(satManifestExists && hasModelUpstreamConsumerUuid){
+          if(results.owner_details.upstreamConsumer.uuid !== modelUpstreamConsumerUuid) {
+            // ERROR: Manifest uuid reported by satellite differs from that on the model
+            // something is corrupt. Assert failure.
+            throw 'ERROR: upstreamConsumer.uuid does not match the one found on the' +
+              'fusor deployment model!';
+          } else {
+            // Existing manifest was found in satellite and matches the one set on the
+            // model by the deployment route, continue with streamlined subs
+            res(true);
+          }
+        } else {
+          // Standard new deployment with no manifest in Sat and with no manifest
+          // having ever been uploaded via the Fusor wizard
+          res(false);
+        }
+      }, () => {
+        rej(false);
+      });
+    });
   },
 
   actions: {
