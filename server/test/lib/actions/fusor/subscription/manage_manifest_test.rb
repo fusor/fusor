74546e1147fece76647f100ccb248e0eb77ca827
Erik Nelson <erik@nsk.io>
2016-06-01 12:38:33 -0400

    Remove tests that no longer apply
    - Entire code branch was removed from the ManageManifest task, these
      tests were designed to test that branch, so they are no longer needed.

--- server/test/lib/actions/fusor/subscription/manage_manifest_test.rb
+++ server/test/lib/actions/fusor/subscription/manage_manifest_test.rb
@@ -21,20 +21,6 @@ module Actions::Fusor::Subscription
       assert_action_planed(@action, ManifestImport)
     end
 
-    test "if org consumer is the same as deployment consumer, schedule refresh" do
-      @deployment.organization.stubs(:owner_details => {'upstreamConsumer' => { 'uuid' => @deployment.upstream_consumer_uuid }})
-      plan_action @action, @deployment, @credentials
-      assert_action_planed(@action, ManifestRefresh)
-    end
-
-    test "if org consumer is the different from deployment consumer, delete old manifest, download new one, and import" do
-      @deployment.organization.stubs(:owner_details => {'upstreamConsumer' => { 'uuid' => 'some other random thing' }})
-      plan_action @action, @deployment, @credentials
-      assert_action_planed(@action, DownloadManifest)
-      assert_action_planed(@action, ManifestDelete)
-      assert_action_planed(@action, ManifestImport)
-    end
-
     test "there is no run method for ManageManifest Actions" do
       @deployment.organization.stubs(:owner_details => {'upstreamConsumer' => ''})
       plan = plan_action @action, @deployment, @credentials
