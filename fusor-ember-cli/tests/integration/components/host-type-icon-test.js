2fd2e5bbd9f944333a4a53c84cd950b0f479e800
Joseph Magen <jmagen@redhat.com>
2016-06-06 17:12:31 +0300

    replace virtual machine icon used in RHEV tables with official PatternFly icon

--- fusor-ember-cli/tests/integration/components/host-type-icon-test.js
+++ fusor-ember-cli/tests/integration/components/host-type-icon-test.js
@@ -5,26 +5,14 @@ moduleForComponent('host-type-icon', 'Integration | Component | host type icon',
   integration: true
 });
 
-test('it shows vm-icon-16 image and hides pficon-screen if isVM is true', function(assert) {
-  this.render(hbs`{{host-type-icon isVM=true}}`);
-  assert.equal(this.$('img').attr('src'), '/assets/r/vm-icon-16.png');
-  assert.notOk(this.$('span.pficon-screen')[0]);
-});
-
-test('it shows vm-icon-16 image and hides pficon-screen if isVM is true and isInverted is false', function(assert) {
-  this.render(hbs`{{host-type-icon isVM=true isInverted=false}}`);
-  assert.equal(this.$('img').attr('src'), '/assets/r/vm-icon-16.png');
-  assert.notOk(this.$('span.pficon-screen')[0]);
-});
-
-test('it shows vm-icon-inverted-16 image and hides pficon-screen if isVM is true and isInverted is true', function(assert) {
-  this.render(hbs`{{host-type-icon isVM=true isInverted=true}}`);
-  assert.equal(this.$('img').attr('src'), '/assets/r/vm-icon-inverted-16.png');
-  assert.notOk(this.$('span.pficon-screen')[0]);
-});
-
-test('it shows pficon-screen icon and hides images if isVM is false', function(assert) {
+test('it shows pficon-screen icon if isVM is false', function(assert) {
   this.render(hbs`{{host-type-icon isVM=false}}`);
   assert.ok(this.$('span.pficon-screen')[0]);
-  assert.notOk(this.$('img').attr('src'));
+  assert.notOk(this.$('span.pficon-virtual-machine')[0]);
+});
+
+test('it shows pficon-virtual-machine icon if isVM is true', function(assert) {
+  this.render(hbs`{{host-type-icon isVM=true}}`);
+  assert.notOk(this.$('span.pficon-screen')[0]);
+  assert.ok(this.$('span.pficon-virtual-machine')[0]);
 });
