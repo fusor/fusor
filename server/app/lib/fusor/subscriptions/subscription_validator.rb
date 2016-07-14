a87fad55d0949f11e2e87928cef94fe80b84ca30
jesus m. rodriguez <jesusr@redhat.com>
2016-07-13 22:52:57 -0400

    use the right quantity when adding sub count.

--- server/app/lib/fusor/subscriptions/subscription_validator.rb
+++ server/app/lib/fusor/subscriptions/subscription_validator.rb
@@ -135,7 +135,7 @@ module Fusor
         added_subscriptions = Fusor::Subscription.where(:deployment_id => id).where(:source => 'added').where('quantity_to_add > 0')
         added_subscriptions.each do |s|
           # no need to add product ids, we're only interested in the additional counts
-          subinfo.update_counts(s.product_name, s.quantity_attached)
+          subinfo.update_counts(s.product_name, s.quantity_to_add)
         end
 
         ::Fusor.log.info "SUB-VAL.build-si-portal: built subscription info from portal. #{subinfo.inspect}"
