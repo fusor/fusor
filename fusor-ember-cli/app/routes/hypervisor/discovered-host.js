f87752e04167a29fdc0a68020d1a0b908b137b59
Chris Chase <chris@chasenc.com>
2016-10-17 14:06:54 -0400

    1385791 - fix underscores in autogenerated self hosted engine name.

--- fusor-ember-cli/app/routes/hypervisor/discovered-host.js
+++ fusor-ember-cli/app/routes/hypervisor/discovered-host.js
@@ -14,8 +14,8 @@ export default Ember.Route.extend(DiscoveredHostRouteMixin, NeedsDiscoveredHosts
     this.set('saveOnTransition', deployment.get('isNotStarted'));
 
     if (deployment.get('isNotStarted') && Ember.isEmpty(deployment.get('rhev_self_hosted_engine_hostname')) && Ember.isPresent(deployment.get('label'))) {
-      let dasherizedLabel = deployment.get('label').trim().replace('_', '-');
-      deployment.set('rhev_self_hosted_engine_hostname', `${dasherizedLabel}-rhev-engine`);
+      let dasherizedLabel = deployment.get('label').trim().replace(/_/g, '-');
+      deployment.set('rhev_self_hosted_engine_hostname', `${dasherizedLabel}-rhv-engine`);
     }
   },
 
